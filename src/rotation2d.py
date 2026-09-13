4898e8e59135c68f2d780482dc62aadc28cdd7a9
ricsirogi <rogozsan.richard06@gmail.com>
2026-09-13 12:18:40 +0200

    Add code for tasks 1, 2

--- src/rotation2d.py
+++ src/rotation2d.py
@@ -1,3 +1,7 @@
 class Rotation2D:
-    def __init__(self):
-        pass
+    def __init__(self, point: tuple[float, float], angle: float):
+        self.point = point
+        self.angle = angle
+
+    def __repr__(self):
+        return "Rotation2D(point=..., angle=...)"
