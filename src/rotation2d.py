1a9c262ffd9280006ad1837ed8ef6a7d8679f1fc
ricsirogi <rogozsan.richard06@gmail.com>
2026-09-20 22:28:44 +0200

    Add docstrings and reorder functions in rotation2d.py

--- src/rotation2d.py
+++ src/rotation2d.py
@@ -5,6 +5,13 @@ import numpy as np
 
 class Rotation2D:
     def __init__(self, point: tuple[float, float] = (0.0, 0.0), angle: float = 0.0):
+        """
+        Initializes a 2D rotation around a center point by a given angle in degrees.
+
+        :param tuple[float, float] point: The center point of rotation (x, y). Defaults to (0.0, 0.0).
+        :param float angle: The rotation angle in degrees. Defaults to 0.0.
+        """
+
         if not (isinstance(point, tuple) and len(point) == 2):
             raise TypeError("Point must be a 2-element tuple.")
         if not isinstance(angle, (int, float)):
@@ -17,14 +24,66 @@ class Rotation2D:
         self.__transformation_matrix = None
         self.__inverse_transformation_matrix = None
 
+    def __repr__(self):
+        """
+        Returns a string representation of the Rotation2D instance.
+
+        :return str repr: Formatted string representing the object.
+        """
+
+        return f"Rotation(point={self.point}, angle={self.angle_deg})"
+
+    def __mul__(self, other: Rotation2D):
+        """
+        Composes two rotations that share the same center point.
+
+        :param Rotation2D other: Another rotation instance to compose with.
+
+        :return Rotation2D result: A new rotation instance representing the combined rotation.
+        """
+
+        if not isinstance(other, Rotation2D):
+            return NotImplemented
+        if other.point == self.point:
+            return Rotation2D(point=self.point, angle=self.angle_deg + other.angle_deg)
+        else:
+            raise ValueError("Rotation points don't match")
+
+    @property
+    def inverse_transformation_matrix(self):
+        """
+        Gets the inverse transformation matrix for this rotation.
+
+        :return np.ndarray matrix: The 3x3 homogeneous matrix representing the inverse rotation.
+        """
+
+        self.__inverse_transformation_matrix = Rotation2D.get_rotation_matrix(-self.angle_rad, point=self.point)
+
+        return self.__inverse_transformation_matrix
+
     @property
     def transformation_matrix(self):
+        """
+        Gets the transformation matrix associated with this rotation.
+
+        :return np.ndarray matrix: The 3x3 homogeneous matrix representing the forward rotation.
+        """
+
         self.__transformation_matrix = Rotation2D.get_rotation_matrix(self.angle_rad, point=self.point)
 
         return self.__transformation_matrix
 
     @staticmethod
     def get_rotation_matrix(angle: float, point: tuple[float, float]) -> np.ndarray:
+        """
+        Calculates the 3x3 homogeneous transformation matrix for a 2D rotation.
+
+        :param float angle: The rotation angle in radians.
+        :param tuple[float, float] point: The center point of rotation (x, y).
+
+        :return np.ndarray matrix: The 3x3 transformation matrix.
+        """
+
         first_matrix = np.array([[1, 0, point[0]],
                                    [0, 1, point[1]],
                                    [0, 0, 1]])
@@ -38,20 +97,3 @@ class Rotation2D:
                                  [0, 0, 1]])
 
         return first_matrix @ second_matrix @ third_matrix
-
-    @property
-    def inverse_transformation_matrix(self):
-        self.__inverse_transformation_matrix = Rotation2D.get_rotation_matrix(-self.angle_rad, point=self.point)
-
-        return self.__inverse_transformation_matrix
-
-    def __repr__(self):
-        return f"Rotation(point={self.point}, angle={self.angle_deg})"
-
-    def __mul__(self, other: Rotation2D):
-        if not isinstance(other, Rotation2D):
-            return NotImplemented
-        if other.point == self.point:
-            return Rotation2D(point=self.point, angle=self.angle_deg + other.angle_deg)
-        else:
-            raise ValueError("Rotation points don't match")
