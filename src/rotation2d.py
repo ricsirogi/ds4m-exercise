9d7287da31cc3a69161003015ab863db90de1c08
ricsirogi <rogozsan.richard06@gmail.com>
2026-09-20 17:11:55 +0200

    Add code for task 3

--- src/rotation2d.py
+++ src/rotation2d.py
@@ -1,7 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
 class Rotation2D:
     def __init__(self, point: tuple[float, float], angle: float):
         self.point = point
         self.angle = angle
+        
+        self.__transformation_matrix = None
+
+    @property
+    def transformation_matrix(self):
+        first_row = [np.cos(self.angle), -np.sin(self.angle)]
+        second_row = [np.sin(self.angle), np.cos(self.angle)]
+
+        self.__transformation_matrix = pd.DataFrame(data=[first_row, second_row])
+
+        return self.__transformation_matrix
 
     def __repr__(self):
         return "Rotation2D(point=..., angle=...)"
+
+
+
